6f51abe5347768c8d6ebf011e2d79dcb1e51535d
JDingo <45366094+JDingo@users.noreply.github.com>
2021-06-03 19:25:59 +0300

    Alusta tehtävä 1.3

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Header = (props) => {
+  console.log(props)
   return (
     <h1>{props.course}</h1>
   )
@@ -35,12 +36,19 @@ const Part = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const part1 = {
+    name: 'Fundementals of React',
+    exercises: 10
+  }
+  const part3 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part2 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
