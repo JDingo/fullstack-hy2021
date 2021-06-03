4234226d4efa609342c3274a3c6b3f5d433a7f48
JDingo <45366094+JDingo@users.noreply.github.com>
2021-06-03 19:30:05 +0300

    Alusta tehtävä 1.4

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -36,19 +36,20 @@ const Part = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-
-  const part1 = {
-    name: 'Fundementals of React',
-    exercises: 10
-  }
-  const part3 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part2 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundementals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   const numofexercises = part1.exercises + part2.exercises + part3.exercises
 
