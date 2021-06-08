97b598b81773daa5eb4420667f0923ad2e14ae51
JDingo <45366094+JDingo@users.noreply.github.com>
2021-06-08 13:05:03 +0300

    Suorita tehtävä 1.9

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -7,6 +7,12 @@ const Statistics = ({ good, neutral, bad }) => {
   const average = (good - bad) / total
   const positive = good / total * 100
 
+  if (total === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
   return (
     <>
       <p>Good {good}</p>
