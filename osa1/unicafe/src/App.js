a3e85799bfc7d4c3d758fbb257e7e4f7965d6929
JDingo <45366094+JDingo@users.noreply.github.com>
2021-06-08 12:47:28 +0300

    Suorita tehtävä 1.6

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,19 @@
 import React, { useState } from 'react'
 
+const Header = ({ text }) => ( <h1>{text}</h1> )
+
+const Button = ({ feedbackType, feedbackFunction, text }) => {
+  return (
+    <button onClick={() => incrementByOne(feedbackType, feedbackFunction)}>
+      {text}
+    </button>
+  )
+}
+
+const Total = ({ total, text }) => ( <p>{text} {total}</p> )
+
+const incrementByOne = (feedbackType, feedbackFunction) => feedbackFunction(feedbackType + 1)
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -8,7 +22,18 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header text='Give feedback' />
+      <div>
+        <Button text='Good' feedbackType={good} feedbackFunction={setGood} />
+        <Button text='Neutral' feedbackType={neutral} feedbackFunction={setNeutral} />
+        <Button text='Bad' feedbackType={bad} feedbackFunction={setBad} />
+      </div>
+      <Header text='Statistics' />
+      <div>
+        <Total total={good} text='Good' />
+        <Total total={neutral} text='Neutral' />
+        <Total total={bad} text='Bad' />
+      </div>
     </div>
   )
 }
