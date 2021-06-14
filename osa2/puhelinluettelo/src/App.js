ac51ac5547bbe270bd92c3883affdd17637b6e03
JDingo <45366094+JDingo@users.noreply.github.com>
2021-06-14 18:52:25 +0300

    Suorita tehtävä 2.9 puhelinluettelon hakukenttä

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244'
-    }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const [ searchFilter, setSearchFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -37,20 +40,35 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchChange = (event) => {
+    setSearchFilter(event.target.value)
+  }
+
+  const entriesToShow = searchFilter.trim() === '' 
+  ? persons 
+  : persons.filter(person => 
+    person.name.toLowerCase().includes(searchFilter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <h3>Search</h3>
+        <div>
+          Filter: <input
+            value={searchFilter}
+            onChange={handleSearchChange}
+            />
+        </div>
+      <h3>Add new entry</h3>
       <form onSubmit={addPerson}>
         <div>
-          name: 
-            <input 
+          Name: <input 
               value={newName}
               onChange={handlePersonChange}  
             />
         </div>
         <div>
-          number: 
-            <input 
+          Number: <input 
               value={newNumber}
               onChange={handleNumberChange}  
             />
@@ -59,9 +77,9 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+        {entriesToShow.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
       </div>
     </div>
   )
