9d019ed011c113f57d613715bcd43b54e521b1ff
JDingo <45366094+JDingo@users.noreply.github.com>
2021-07-14 13:54:01 +0300

    Suorita tehtävät 4.4 totalLikes ja 4.5 favouriteBlog

---
+++ osa4/blogilista/tests/list_helper.test.js
@@ -0,0 +1,84 @@
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+    const blogs = []
+
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+})
+
+const emptyList = []
+
+const listWithOneBlog = [
+    {
+        _id: '60ee9df50d364c3c2814ca79',
+        title: 'Koirablogi',
+        author: 'Koira Keksi',
+        url: 'koire.fi',
+        likes: 5,
+        __v: 0
+    }
+]
+
+const listWithManyBlogs = [
+    {
+        _id: '60ee9df50d364c3c2814ca79',
+        title: 'Koirablogi',
+        author: 'Koira Keksi',
+        url: 'koire.fi',
+        likes: 1,
+        __v: 0
+    },
+    {
+        _id: '60eb9df50d364c3c2814ca79',
+        title: 'Kissablogi',
+        author: 'Kissa Kallu',
+        url: 'kassi.fi',
+        likes: 1000,
+        __v: 0
+    },
+    {
+        _id: '60ec9df50d364c3c2814ca79',
+        title: 'Lisko',
+        author: 'Lisko Liimatta',
+        url: 'sisisliskot.fi',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '60ed9df50d364c3c2814ca79',
+        title: 'Alpakka',
+        author: 'Alpakka Allu',
+        url: 'Alpakat.io',
+        likes: 5123,
+        __v: 0
+    },
+    {
+        _id: '60eb9df50d364c3c2814ca79',
+        title: 'Kissablogi 2',
+        author: 'Kissa Kallu',
+        url: 'kassi2.fi',
+        likes: 5000,
+        __v: 0
+    }
+]
+
+describe('total likes', () => {
+    test('of empty list is zero', () => {
+        expect(listHelper.totalLikes(emptyList)).toBe(0)
+    })
+
+    test('when list has only one blog equals to the likes of that blog', () => {
+        expect(listHelper.totalLikes(listWithOneBlog)).toBe(5)
+    })
+
+    test('of a bigger list is calculated right', () => {
+        expect(listHelper.totalLikes(listWithManyBlogs)).toBe(11129)
+    })
+})
+
+describe('favourite blog', () => {
+    test('when the most liked count is 5123', () => {
+        expect(listHelper.favouriteBlog(listWithManyBlogs)).toEqual(listWithManyBlogs[3])
+    })
+})
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,33 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (likesAccumulator, currentBlog) => likesAccumulator + currentBlog.likes
+
+    if (blogs.length === 0) {
+        return 0
+    } else {
+        return blogs.reduce(reducer, 0)
+    }
+}
+
+const favouriteBlog = (blogs) => {
+    const reducer = (currentFavourite, currentBlog) => {
+        if (currentBlog.likes > currentFavourite.likes) {
+            return currentBlog
+        } else {
+            return currentFavourite
+        }
+    }
+
+    return blogs.reduce(reducer)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favouriteBlog
 }
