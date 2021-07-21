9d6d0e91ff7e7e0f8dc00c9acacdd62600c64d5f
JDingo <45366094+JDingo@users.noreply.github.com>
2021-07-21 16:46:28 +0300

    Suorita tehtävä 4.17 populate

--- osa4/blogilista/models/user.js
+++ osa4/blogilista/models/user.js
@@ -9,7 +9,13 @@ const userSchema = mongoose.Schema({
         unique: true
     },
     name: String,
-    password: String
+    password: String,
+    blogs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Blog'
+        }
+    ]
 })
 
 userSchema.plugin(uniqueValidator)
