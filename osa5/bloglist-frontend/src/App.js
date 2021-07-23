0c0b6f8dd377ac8ab005374fbc9cfde8767bc9f5
JDingo <45366094+JDingo@users.noreply.github.com>
2021-07-23 15:56:26 +0300

    Suoritat tehtävä 5.3 kirjautunut voi luoda blogeja

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
 import LoginForm from './components/LoginForm'
 import BlogForm from './components/BlogForm'
+import BlogList from './components/BlogList'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
 
-  const [username, setUsername] = useState([])
-  const [password, setPassword] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -23,6 +27,7 @@ const App = () => {
     if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
+      blogService.setToken(user.token)
     }
   }, [])
 
@@ -37,6 +42,7 @@ const App = () => {
         'loggedUser', JSON.stringify(user)
       )
 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -48,6 +54,7 @@ const App = () => {
 
   const handleLogout = () => {
     window.localStorage.removeItem('loggedUser')
+    blogService.setToken(null)
     setUser(null)
     setUsername('')
     setPassword('')
@@ -61,6 +68,38 @@ const App = () => {
     setPassword(event.target.value)
   }
 
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value)
+  }
+
+  const addBlog = async (event) => {
+    event.preventDefault()
+
+    try {
+      const blog = await blogService.create({
+        title, author, url
+      })
+
+      blogService.getAll().then(blogs =>
+        setBlogs(blogs)
+      )
+
+      setTitle('')
+      setAuthor('')
+      setUrl('')
+    } catch (exception) {
+      console.log(exception)
+    }
+  }
+
   return (
     <div>
       {user === null ?
@@ -71,11 +110,19 @@ const App = () => {
           password={password}
           handlePasswordChange={handlePasswordChange}
         /> :
-        <BlogForm
-          displayName={user.name}
-          blogs={blogs}
-          handleLogout={handleLogout}
-        />
+        <div>
+          <BlogForm
+            addBlog={addBlog}
+            blogValueAndHandlers={
+              { title, handleTitleChange, author, handleAuthorChange, url, handleUrlChange }
+            }
+            displayName={user.name}
+            handleLogout={handleLogout}
+          />
+          <BlogList
+            blogs={blogs}
+          />
+        </div>
       }
     </div>
   )
--- osa5/bloglist-frontend/src/components/BlogForm.js
+++ osa5/bloglist-frontend/src/components/BlogForm.js
@@ -1,18 +1,42 @@
 import React from 'react'
-import Blog from './Blog'
 
-const BlogForm = ({ displayName, blogs, handleLogout }) => {
-    const logoutButton = (handleLogout) => (
-        <button onClick={handleLogout}>Log out</button>
-    )
-
+const BlogForm = ({ displayName, blogValueAndHandlers, handleLogout, addBlog }) => {
     return (
         <div>
             <h2>Blogs</h2>
-            <p>{displayName} logged in {logoutButton(handleLogout)}</p>
-            {blogs.map(blog =>
-                <Blog key={blog.id} blog={blog} />
-            )}
+            <p>{displayName} logged in <button onClick={handleLogout}>Log out</button></p>
+
+            <form onSubmit={addBlog}>
+                <div>
+                    Title:
+                    <input
+                        type="text"
+                        value={blogValueAndHandlers.title}
+                        name="Title"
+                        onChange={blogValueAndHandlers.handleTitleChange}
+                    />
+                </div>
+                <div>
+                    Author:
+                    <input
+                        type="author"
+                        value={blogValueAndHandlers.author}
+                        name="Author"
+                        onChange={blogValueAndHandlers.handleAuthorChange}
+                    />
+                </div>
+                <div>
+                    Url:
+                    <input
+                        type="text"
+                        value={blogValueAndHandlers.url}
+                        name="Url"
+                        onChange={blogValueAndHandlers.handleUrlChange}
+                    />
+                </div>
+            </form>
+
+            <button onClick={addBlog}>Create</button>
         </div>
     )
 }
