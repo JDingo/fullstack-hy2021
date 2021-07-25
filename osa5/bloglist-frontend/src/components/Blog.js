3847ace4ab5a96740aeb26b930d59a64449f931f
JDingo <45366094+JDingo@users.noreply.github.com>
2021-07-25 17:06:20 +0300

    Suorita tehtävä 5.7 yksittäisen blogin tietojen laajennus

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+import '../index.css'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const buttonText = visible ? 'Hide' : 'Show'
+
+  const blogStyle = {
+    borderStyle: 'solid',
+    borderRadius: '3px',
+    padding: '5px',
+    margin: '5px'
+  }
+
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} <button onClick={() => toggleVisibility()}>{buttonText}</button> <br/>
+        {blog.url} <br/>
+        {blog.likes} <button>Like</button> <br/>
+        {blog.author} <br/>
+      </div >
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} By {blog.author}
+        <button onClick={() => toggleVisibility()}>{buttonText}</button>
+      </div>
+    )
+  }
+}
 
 export default Blog
