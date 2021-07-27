94bb43087630be0066e024c4d26e9e2f143131c8
JDingo <45366094+JDingo@users.noreply.github.com>
2021-07-27 14:52:33 +0300

    Suorita tehtävä 5.14 testaa kaikkien tietojen näyttäminen blogin avaamisen jälkeen

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,25 +1,44 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from './Blog'
 
-test('renders blog at first with only title and author', () => {
-    const blog = {
-        title: 'Test Title',
-        author: 'Test Author',
-        url: 'Test.url',
-        likes: 0,
-        user: {
-            username: 'Tester'
+describe('<Blog />', () => {
+    let component
+
+    beforeEach(() => {
+        const blog = {
+            title: 'Test Title',
+            author: 'Test Author',
+            url: 'Test.url',
+            likes: 0,
+            user: {
+                username: 'Tester'
+            }
         }
-    }
 
-    const component = render (
-        <Blog blog={blog} username={'Tester'} />
-    )
+        const mockLikeHandler = jest.fn()
+        const mockDeleteHandler = jest.fn()
 
-    expect(component.container).toHaveTextContent('Test Title')
-    expect(component.container).toHaveTextContent('Test Author')
-    expect(component.container).not.toHaveTextContent('Test.url')
-    expect(component.container).not.toHaveTextContent('0')
+        component = render(
+            <Blog blog={blog} username={'Tester'} handleLike={mockLikeHandler} handleDelete={mockDeleteHandler} />
+        )
+    })
+
+    test('renders blog at start with only title and author', () => {
+        expect(component.container).toHaveTextContent('Test Title')
+        expect(component.container).toHaveTextContent('Test Author')
+        expect(component.container).not.toHaveTextContent('Test.url')
+        expect(component.container).not.toHaveTextContent('0')
+    })
+
+    test('renders blog with full info after opened', () => {
+        const button = component.getByText('Show')
+        fireEvent.click(button)
+
+        expect(component.container).toHaveTextContent('Test Title')
+        expect(component.container).toHaveTextContent('Test Author')
+        expect(component.container).toHaveTextContent('Test.url')
+        expect(component.container).toHaveTextContent('0')
+    })
 })
