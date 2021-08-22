03893a3d2d50c514b6fcde4fd8bdac2014ca461d
JDingo <45366094+JDingo@users.noreply.github.com>
2021-08-22 20:32:27 +0300

    Suorita tehtävä 6.4 lisää mahdollisuus uusille anekdooteille

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -26,6 +26,17 @@ export const voteAnecdote = (id) => {
     }
 }
 
+export const createAnecdote = (content) => {
+    return {
+        type: 'NEW_ANECDOTE',
+        data: {
+            content,
+            id: getId(),
+            votes: 0
+        }
+    }
+}
+
 const reducer = (state = initialState, action) => {
     console.log('state now: ', state)
     console.log('action', action)
@@ -42,6 +53,8 @@ const reducer = (state = initialState, action) => {
             return state.map(anecdote =>
                 anecdote.id !== id ? anecdote : updatedAnecdote
             )
+        case 'NEW_ANECDOTE':
+            return [...state, action.data]
         default:
             return state
     }
