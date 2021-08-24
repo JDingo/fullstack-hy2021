ab86590ea5ce6e054c9462405984e93d22ba8cf1
JDingo <45366094+JDingo@users.noreply.github.com>
2021-08-24 15:25:46 +0300

    Suorita tehtävä 6.14 anekdoottien tallennus backendiin

--- osa6/redux-anecdotes/src/components/AnecdoteForm.js
+++ osa6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { removeNotification, setCreationNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
 
-    const addAnecdote = (event) => {
+    const addAnecdote = async (event) => {
         event.preventDefault()
+
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        dispatch(createAnecdote(content))
+
+        const newNote = await anecdoteService.createNew(content)
+
+        dispatch(createAnecdote(newNote))
         dispatch({ type: 'SORT' })
         dispatch(setCreationNotification(content))
 
