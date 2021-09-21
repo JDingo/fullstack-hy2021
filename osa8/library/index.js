891667ace92dd65198928933a7f623dee39a2dcc
JDingo <45366094+JDingo@users.noreply.github.com>
2021-09-21 20:47:17 +0300

    Tehtävä 8.7 Kirjailijan syntymävuoden päivitys

--- osa8/library/index.js
+++ osa8/library/index.js
@@ -114,6 +114,11 @@ const typeDefs = gql`
       author: String!
       genres: [String!]
     ): Book
+
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -154,6 +159,18 @@ const resolvers = {
       }
 
       return book
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+
+      if (!author) {
+        return null
+      }
+
+      author.born = args.setBornTo
+
+      return author
     }
   }
 }
