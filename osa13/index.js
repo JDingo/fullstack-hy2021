decef4a8260dfaf6b73e40d87a80b8c4ec79467c
JDingo <45366094+JDingo@users.noreply.github.com>
2022-03-03 17:07:41 +0200

    Exercise 13.4. api endpoints and testing requests

--- osa13/index.js
+++ osa13/index.js
@@ -1,7 +1,70 @@
 require('dotenv').config()
-const { Sequelize } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+});
+
+class Blog extends Model { }
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER
+  }
+},
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog'
+  })
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+    await blog.destroy()
+    res.sendStatus(200)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
