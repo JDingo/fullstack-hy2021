c6ae9b138f0eef46f8222224608572576a805d73
JDingo <45366094+JDingo@users.noreply.github.com>
2022-03-06 20:47:51 +0200

    Exercise 13.5. structure files

---
+++ osa13/controllers/blogs.js
@@ -0,0 +1,29 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    res.json(blog)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+    await blog.destroy()
+    res.sendStatus(200)
+  } catch (error) {
+    res.status(400).json({ error })
+  }
+})
+
+module.exports = router
--- osa13/index.js
+++ osa13/index.js
@@ -1,72 +1,19 @@
-require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+
 const app = express()
 app.use(express.json())
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-});
+const blogsRouter = require('./controllers/blogs')
 
-class Blog extends Model { }
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT,
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER
-  }
-},
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: 'blog'
+app.use('/api/blogs', blogsRouter)
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
   })
+}
 
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll()
-  res.json(blogs)
-})
-
-app.post('/api/blogs', async (req, res) => {
-  try {
-    const blog = await Blog.create(req.body)
-    res.json(blog)
-  } catch (error) {
-    res.status(400).json({ error })
-  }
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  try {
-    const blog = await Blog.findByPk(req.params.id)
-    await blog.destroy()
-    res.sendStatus(200)
-  } catch (error) {
-    res.status(400).json({ error })
-  }
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+start()
