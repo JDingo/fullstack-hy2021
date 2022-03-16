4cc89423305d236d769286dba28b7a4763252de5
JDingo <45366094+JDingo@users.noreply.github.com>
2022-03-16 16:30:02 +0200

    Exercise 13.22. end point for toggling read status

--- osa13/controllers/readingLists.js
+++ osa13/controllers/readingLists.js
@@ -1,4 +1,5 @@
-const { ReadingList } = require('../models')
+const { ReadingList, User } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 const router = require('express').Router()
 
@@ -10,6 +11,22 @@ router.post('/', async (req, res, next) => {
     next(error)
     res.status(400).json({ error: error.errors[0].message })
   }
+})
+
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.decodedToken.id)
+
+    const readBlog = await ReadingList.findOne({
+      where: {
+        userId: user.id, blogId: req.params.id
+      }
+    })
+
+    readBlog.readStatus = req.body.read
+    readBlog.save()
+    res.json(readBlog)
+  } catch (error) { next(error) }
 })
 
 module.exports = router
